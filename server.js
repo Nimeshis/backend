dd9f1d91ae2b334abfd77be149b7b44210111769
Nimeshis <dhungelnimesh5@gmail.com>
2024-08-29 16:57:05 +0545

    connection string changes

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 
 // Connect to MongoDB
+// Configure MongoDB URI
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TestDb";
+
+// Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/testing", {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1); // Exit the process with a failure
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
   });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
 
